0a73c72a37ba86175dd0092eafd1f4561205fe4b
krgneha <neha224229@gmail.com>
2021-05-18 12:51:56 +0530

    final code

--- 01_Array_Basic/01_Array_Intro.cpp
+++ 01_Array_Basic/01_Array_Intro.cpp
@@ -2,8 +2,47 @@
 using namespace std;
 
 int main(){
-    int a[10];
-   a[0] = 2;
-   a[4] = 5;
-   cout<<a[0]<<" "<<a[4]<<endl;
+//     int a[10];
+//    a[0] = 2;
+//    a[4] = 5;
+//    cout<<a[0]<<" "<<a[4]<<endl;
+
+// int a[4]={1,2,3,4};
+// for(int i=0;i<4;i++){
+//     cout<<a[i]<<" ";
+
+// }
+// cout<<endl;
+// cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<endl;
+
+// int a[10];
+// for(int i=0;i<10;i++){
+//     cout<<a[i]<<" ";
+// }
+
+// int a[10]={2};
+// for(int i=0;i<10;i++){
+//     cout<<a[i]<<" ";
+// }
+
+// int a[10]={0};
+// for(int i=0;i<10;i++){
+//     cout<<a[i]<<" ";
+// }
+
+// int a[10];
+// for(int i=0;i<10;i++){
+//     a[i]=i*i*i;
+//     cout<<a[i]<<" ";
+// }
+
+// cout<<endl;
+
+int d[10]={1,2,3,4};
+cout<<sizeof(d);
+return 0;
+
+
+
+
 }
